20ed5e0db6dda790b577f515b33643e694f62885
vcow10kar <vkawthankar@gmail.com>
2021-12-15 11:19:56 +0530

    added fonts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import './App.css';
-import { Button } from '@mui/material';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 function App() {
   return (
     <div className="App">
-    <div>
-      <div>
-        <Button disableElevation variant="contained">ADD TO CART</Button>
-      </div>
 
-      <br />
-      <Button disableElevation variant="contained" startIcon={<ArrowForwardIcon />}>GO TO CART</Button>
-      </div>
     </div>
   );
 }
