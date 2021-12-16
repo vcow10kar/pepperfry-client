2da87a6b9b0774596379187de42be89b1aa38bcf
vcow10kar <vkawthankar@gmail.com>
2021-12-16 20:15:18 +0530

    All products routes added

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -3,13 +3,14 @@ import SpecificProductPage  from '../components/SpecificProductPage/SpecificProd
 import {Main as Cart} from '../components/Cart/Main';
 import {AddressCard} from '../components/deliveryAddressPage/AddressCard';
 import Home from '../components/Home/Home';
+import AllproductPage from '../allProductPage/AllproductPage';
 
 const AppRoutes = () => {
     return (
         <div>
             <Switch>
                 <Route path = '/' exact component={Home}/>
-                <Route path = '/allProducts' component={Home}/>
+                <Route path = '/allProducts' component={AllproductPage}/>
                 <Route path = '/allProducts/:id' component={SpecificProductPage}/>
                 <Route path = '/cart' component={Cart}/>
                 <Route path = '/address' component = {AddressCard}/>
