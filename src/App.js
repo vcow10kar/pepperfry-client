7821abe2b55dedc343704647a5c3e4414fb0a649
vcow10kar <vkawthankar@gmail.com>
2021-12-16 19:42:15 +0530

    css fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-
+import {Main as Cart} from './components/Cart/Main';
+import SpecificProductPage from './components/SpecificProductPage/SpecificProductPage';
 
 function App() {
   return (
     <div className="App">
-
+      <Cart/>
+      {/* <SpecificProductPage/> */}
     </div>
   );
 }
