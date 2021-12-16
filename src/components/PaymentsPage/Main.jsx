74f18e87cab77014abbde88c5fbd15a0d1a69ba9
madhuvenkat <madhuvenkatmedicharla@gmail.com>
2021-12-17 03:10:30 +0530

    payments page

--- src/components/PaymentsPage/Main.jsx
+++ src/components/PaymentsPage/Main.jsx
@@ -1,19 +1,23 @@
 // import { Footer } from "../deliveryAddressPage/footer"
+import { Footer } from "../deliveryAddressPage/footer"
 import { BillingCard } from "./BillingCard/BillingCard"
 import { CartCard } from "./CartCard/CartCard"
+import { NavBar } from "./Nav/Nav"
 import { OrderSummeryCard } from "./OrderSummeryCard/OrderSummeryCard"
 import { PaymentsCard } from "./PaymentCard/PaymentsCard"
 
 export const PaymentsPage = () => {
-    return<div style={{display:"flex", flexDirection:'column'}}>
+    return <div >
+        <NavBar />
         <div>
-        <OrderSummeryCard/>
-        <BillingCard/>
-        <PaymentsCard/>
+            <OrderSummeryCard />
+            <BillingCard />
+            <PaymentsCard />
+
+            <div>
+                <CartCard />
+            </div>
         </div>
-        <div>
-        <CartCard/>
-        </div>
-       
+        <Footer />
     </div>
 }
