9035be6d2f7396f77139d42395944db47dea9719
vcow10kar <vkawthankar@gmail.com>
2021-12-16 18:47:00 +0530

    App.js corrected

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import SpecificProductPage from './components/SpecificProductPage/SpecificProductPage';
+
 
 function App() {
   return (
     <div className="App">
-      <SpecificProductPage/>
+
     </div>
   );
 }
