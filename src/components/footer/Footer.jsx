b2d510889710405f2ba766e660abd8b5a593c307
mukhiarambhu <rambhumukhia@gmail.com>
2021-12-17 20:56:40 +0530

    Navbar added and footer alignment corrected

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,165 +4,169 @@ import styles from "./footer.module.css";
 function Footer() {
   return (
     <>
-      <div className={styles.mainFooterDiv}>
-        <div className={styles.subdiv1}>
-          <div className={styles.subdiv11}>
-            <h4>Pepperfry</h4>
-            <p>About us</p>
-            <p>Carrers</p>
-            <p>Corporate governance</p>
-            <p> pepperfry in the news</p>
-            <p>Find a studio</p>
-            <p>Gift cards</p>
-            <p>Brands</p>
-            <p>Customers reviews</p>
-          </div>
-          <div className={styles.subdiv12}>
-            <h6>Partners</h6>
-            <p>Become a franchise</p>
-            <p>Design for us</p>
-            <p>Pepperfry marketplace policies</p>
-            <p>Login to your merchant dashboard</p>
-            <p>
-              {" "}
-              <span>Importnat:</span>GST and you
-            </p>
-            <p>Corporate enquiries</p>
-          </div>
-          <div className={styles.subdiv13}>
-            <h6>NEED HELP?</h6>
-            <p>Contact us</p>
-            <p>Return & refund</p>
-            <p>Track your order</p>
-            <p>FAQ</p>
-            <p>Buy on phone</p>
-          </div>
-        </div>
-        <div className={styles.subDiv2}>
-          <div className={styles.subdiv21}>
-            <h6>POPULAR CATEGORIES</h6>
-            <p>Queen size beds, King size beds, Coffee tables, Dining,</p>{" "}
-            <p>Sets, Recliners, Sofacum beds, Rocking Chairs , Cabinets,</p>{" "}
-            <p>Book shelves, TV unit, Wardrobes, Outdoor Furniture,</p>
-            <p>Bar cabinets, Wall shelves, Photo frames, Bedsheets,</p>
-            <p>Mattresses, Table Cloth, Living room furniture, Study</p>
-            <p>Table, Dining room furniture, Office furniture,Bed</p>
-            <p>Room furniture,Dining table, Beds, Sofas</p>
-          </div>
-          <div className={styles.subdiv22}>
-            <h6>POPULAR BRANDS</h6>
-            <p>Mintwud, Woodscraft ,Casacraft, Amberville</p>
-            <p>Mudramark, Bohemiana, Primorati, Mollycoddle,</p>
-            <p>Mangiamo, Clouddio, Spacewood, Durian, Star </p>
-            <p>India, Adiko Systems, Crystal Furnitech, Springtek,</p>
-            <p>Story@Homes, Parin, Furnitech, Trevi Furniture, </p>
-            <p>Peachtree, Durfi, Muebles Casa, Duroflex</p>
-          </div>
-          <div className={styles.subdiv23}>
-            <h6>CITIES WE DELIVER TO</h6>
-            <p>Bengaluru, Mumbai, Navi Mumbai, Delhi, Hyderabad,</p>
-            <p>Pune, Chennai, Gurgaon, Kolkata, Noida, Goa, Ghaziabad,</p>
-            <p>Ahmedabad, Coimbatore, Faridabad, Jaipur, Lucknow,</p>
-            <p>Kochi, Vishakhapatnam, Chandigarh, Vadodara, Nagpur</p>
-            <p>Thiruvananthapuram, Indore, Mysore, Bhopal, Surat,</p>
-            <p>Jalandhar, Patna, Ludhiana, Nashik, Madurai, Kanpur,</p>
-            <p>Aurangabad, Imphal, Hubli, Guwahati and Many more</p>
-          </div>
-        </div>
-        <div className={styles.subDiv3}>
-          <div className={styles.subDiv31}>
-            <h6 style={{ marginBottom: "20px" }}>WE ACCEPT</h6>
-            <div className={styles.subDiv311}>
-              <img
-                style={{ marginLeft: "0px" }}
-                src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/363_Visa_Credit_Card_logo-512.png"
-                alt="visa"
-              />
-              <img
-                src="https://cdn0.iconfinder.com/data/icons/credit-card-debit-card-payment-PNG/128/Mastercard-Curved.png"
-                alt="visa"
-              />
-              <img
-                src="https://cdn3.iconfinder.com/data/icons/payment-methods-18/512/card_credit_vector_Maestro-1024.png"
-                alt="visa"
-              />
-              <img
-                src="https://cdn2.iconfinder.com/data/icons/credit-cards-6/156/american_express-1024.png"
-                alt="visa"
-              />
+      <div style={{ width: "100%", backgroundColor: "#F3F5F7" }}>
+        <div className={styles.mainFooterDiv}>
+          <div className={styles.subdiv1}>
+            <div className={styles.subdiv11}>
+              <h4>Pepperfry</h4>
+              <p>About us</p>
+              <p>Carrers</p>
+              <p>Corporate governance</p>
+              <p> pepperfry in the news</p>
+              <p>Find a studio</p>
+              <p>Gift cards</p>
+              <p>Brands</p>
+              <p>Customers reviews</p>
             </div>
-            <div className={styles.subDiv312}>
-              <img
-                style={{ marginLeft: "0px" }}
-                src="https://cdn0.iconfinder.com/data/icons/payment-method/480/rupay_payment_card_bank-1024.png"
-                alt="rupeecard"
-              />
-              <img
-                src="https://cdn0.iconfinder.com/data/icons/major-credit-cards-colored/48/JD-07-1024.png"
-                alt="rupeecard"
-              />
-              <img
-                src="https://cdn3.iconfinder.com/data/icons/payment-method-1/64/_Diners_Club_International-1024.png"
-                alt="rupeecard"
-              />
-              <img
-                src="https://cdn2.iconfinder.com/data/icons/credit-cards-6/156/visa-1024.png"
-                alt="visa card"
-              />
-              <img
-                src="https://cdn0.iconfinder.com/data/icons/credit-8/512/27_credit-1024.png"
-                alt="card"
-              />
+            <div className={styles.subdiv12}>
+              <h6>Partners</h6>
+              <p>Become a franchise</p>
+              <p>Design for us</p>
+              <p>Pepperfry marketplace policies</p>
+              <p>Login to your merchant dashboard</p>
+              <p>
+                {" "}
+                <span>Importnat:</span>GST and you
+              </p>
+              <p>Corporate enquiries</p>
+            </div>
+            <div className={styles.subdiv13}>
+              <h6>NEED HELP?</h6>
+              <p>Contact us</p>
+              <p>Return & refund</p>
+              <p>Track your order</p>
+              <p>FAQ</p>
+              <p>Buy on phone</p>
             </div>
           </div>
-          <div className={styles.subDiv32}>
-            <h6 style={{ marginBottom: "20px" }}>DOWNLOAD OUR APP</h6>
-            <div className={styles.subDiv321}>
-              <img
-                style={{ marginLeft: "0px" }}
-                src="https://www.netmeds.com/assets/gloryweb/images/icons/app_store.png"
-                alt="istore"
-              />
-              <img
-                src="https://www.netmeds.com/assets/gloryweb/images/icons/play_store.png"
-                alt="android"
-              />
+          <div className={styles.subDiv2}>
+            <div className={styles.subdiv21}>
+              <h6>POPULAR CATEGORIES</h6>
+              <p>
+                Queen size beds, King size beds, Coffee tables, Dining,
+              </p>{" "}
+              <p>Sets, Recliners, Sofacum beds, Rocking Chairs , Cabinets,</p>{" "}
+              <p>Book shelves, TV unit, Wardrobes, Outdoor Furniture,</p>
+              <p>Bar cabinets, Wall shelves, Photo frames, Bedsheets,</p>
+              <p>Mattresses, Table Cloth, Living room furniture, Study</p>
+              <p>Table, Dining room furniture, Office furniture,Bed</p>
+              <p>Room furniture,Dining table, Beds, Sofas</p>
+            </div>
+            <div className={styles.subdiv22}>
+              <h6>POPULAR BRANDS</h6>
+              <p>Mintwud, Woodscraft ,Casacraft, Amberville</p>
+              <p>Mudramark, Bohemiana, Primorati, Mollycoddle,</p>
+              <p>Mangiamo, Clouddio, Spacewood, Durian, Star </p>
+              <p>India, Adiko Systems, Crystal Furnitech, Springtek,</p>
+              <p>Story@Homes, Parin, Furnitech, Trevi Furniture, </p>
+              <p>Peachtree, Durfi, Muebles Casa, Duroflex</p>
+            </div>
+            <div className={styles.subdiv23}>
+              <h6>CITIES WE DELIVER TO</h6>
+              <p>Bengaluru, Mumbai, Navi Mumbai, Delhi, Hyderabad,</p>
+              <p>Pune, Chennai, Gurgaon, Kolkata, Noida, Goa, Ghaziabad,</p>
+              <p>Ahmedabad, Coimbatore, Faridabad, Jaipur, Lucknow,</p>
+              <p>Kochi, Vishakhapatnam, Chandigarh, Vadodara, Nagpur</p>
+              <p>Thiruvananthapuram, Indore, Mysore, Bhopal, Surat,</p>
+              <p>Jalandhar, Patna, Ludhiana, Nashik, Madurai, Kanpur,</p>
+              <p>Aurangabad, Imphal, Hubli, Guwahati and Many more</p>
             </div>
           </div>
-          <div className={styles.subDiv33}>
-            <div className={styles.subDiv331}>
+          <div className={styles.subDiv3}>
+            <div className={styles.subDiv31}>
+              <h6 style={{ marginBottom: "20px" }}>WE ACCEPT</h6>
+              <div className={styles.subDiv311}>
+                <img
+                  style={{ marginLeft: "0px" }}
+                  src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/363_Visa_Credit_Card_logo-512.png"
+                  alt="visa"
+                />
+                <img
+                  src="https://cdn0.iconfinder.com/data/icons/credit-card-debit-card-payment-PNG/128/Mastercard-Curved.png"
+                  alt="visa"
+                />
+                <img
+                  src="https://cdn3.iconfinder.com/data/icons/payment-methods-18/512/card_credit_vector_Maestro-1024.png"
+                  alt="visa"
+                />
+                <img
+                  src="https://cdn2.iconfinder.com/data/icons/credit-cards-6/156/american_express-1024.png"
+                  alt="visa"
+                />
+              </div>
+              <div className={styles.subDiv312}>
+                <img
+                  style={{ marginLeft: "0px" }}
+                  src="https://cdn0.iconfinder.com/data/icons/payment-method/480/rupay_payment_card_bank-1024.png"
+                  alt="rupeecard"
+                />
+                <img
+                  src="https://cdn0.iconfinder.com/data/icons/major-credit-cards-colored/48/JD-07-1024.png"
+                  alt="rupeecard"
+                />
+                <img
+                  src="https://cdn3.iconfinder.com/data/icons/payment-method-1/64/_Diners_Club_International-1024.png"
+                  alt="rupeecard"
+                />
+                <img
+                  src="https://cdn2.iconfinder.com/data/icons/credit-cards-6/156/visa-1024.png"
+                  alt="visa card"
+                />
+                <img
+                  src="https://cdn0.iconfinder.com/data/icons/credit-8/512/27_credit-1024.png"
+                  alt="card"
+                />
+              </div>
+            </div>
+            <div className={styles.subDiv32}>
               <h6 style={{ marginBottom: "20px" }}>DOWNLOAD OUR APP</h6>
-              <img
-                style={{ marginLeft: "0px" }}
-                src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/facebook-1024.png"
-                alt="facebook"
-              />
-              <img
-                src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-1024.png"
-                alt="instagram"
-              />
-              <img
-                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/pinterest-1024.png"
-                alt="pintrest"
-              />
-              <img
-                src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Twitter2_colored_svg-1024.png"
-                alt="twitter"
-              />
-              <img
-                src="https://cdn1.iconfinder.com/data/icons/logotypes/32/youtube-1024.png"
-                alt="youtube"
-              />
+              <div className={styles.subDiv321}>
+                <img
+                  style={{ marginLeft: "0px" }}
+                  src="https://www.netmeds.com/assets/gloryweb/images/icons/app_store.png"
+                  alt="istore"
+                />
+                <img
+                  src="https://www.netmeds.com/assets/gloryweb/images/icons/play_store.png"
+                  alt="android"
+                />
+              </div>
+            </div>
+            <div className={styles.subDiv33}>
+              <div className={styles.subDiv331}>
+                <h6 style={{ marginBottom: "20px" }}>DOWNLOAD OUR APP</h6>
+                <img
+                  style={{ marginLeft: "0px" }}
+                  src="https://cdn0.iconfinder.com/data/icons/social-flat-rounded-rects/512/facebook-1024.png"
+                  alt="facebook"
+                />
+                <img
+                  src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Instagram_colored_svg_1-1024.png"
+                  alt="instagram"
+                />
+                <img
+                  src="https://cdn1.iconfinder.com/data/icons/logotypes/32/pinterest-1024.png"
+                  alt="pintrest"
+                />
+                <img
+                  src="https://cdn2.iconfinder.com/data/icons/social-media-2285/512/1_Twitter2_colored_svg-1024.png"
+                  alt="twitter"
+                />
+                <img
+                  src="https://cdn1.iconfinder.com/data/icons/logotypes/32/youtube-1024.png"
+                  alt="youtube"
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles.subDiv332}>
-          <div className={styles.subDiv3321}>
-            Buy In Bulk | Write a testimonial
-          </div>
-          <div className={styles.subDiv3322}>
-            Whitehat | Site Map | Terms Of Use | Privacy Policy | Your Data &
-            Security | GrievanceRedressal
+          <div className={styles.subDiv332}>
+            <div className={styles.subDiv3321}>
+              Buy In Bulk | Write a testimonial
+            </div>
+            <div className={styles.subDiv3322}>
+              Whitehat | Site Map | Terms Of Use | Privacy Policy | Your Data &
+              Security
+            </div>
           </div>
         </div>
       </div>
