5ec485113651e36fbd2186e2ef4de09567b3a6e5
mukhiarambhu <rambhumukhia@gmail.com>
2021-12-17 11:20:30 +0530

    all pending push

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,22 +1,24 @@
-import {Switch, Route} from 'react-router-dom';
-import SpecificProductPage  from '../components/SpecificProductPage/SpecificProductPage';
-import {Main as Cart} from '../components/Cart/Main';
-import {AddressCard} from '../components/deliveryAddressPage/AddressCard';
-import Home from '../components/Home/Home';
-import AllproductPage from '../allProductPage/AllproductPage';
+import { Switch, Route } from "react-router-dom";
+import SpecificProductPage from "../components/SpecificProductPage/SpecificProductPage";
+import { Main as Cart } from "../components/Cart/Main";
+import { AddressCard } from "../components/deliveryAddressPage/AddressCard";
+import Home from "../components/Home/Home";
+import AllproductPage from "../allProductPage/AllproductPage";
+// import SearchNavbar from "../components/Navbar/SearchNavbar";
 
 const AppRoutes = () => {
-    return (
-        <div>
-            <Switch>
-                <Route path = '/' exact component={Home}/>
-                <Route path = '/allProducts' component={AllproductPage}/>
-                <Route path = '/products/:id' component={SpecificProductPage}/>
-                <Route path = '/cart' component={Cart}/>
-                <Route path = '/address' component = {AddressCard}/>
-            </Switch>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/allProducts" component={AllproductPage} />
+        <Route path="/products/:id" component={SpecificProductPage} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/address" component={AddressCard} />
+        {/* <Route path="/testing">{SearchNavbar}</Route> */}
+      </Switch>
+    </div>
+  );
+};
 
 export default AppRoutes;
