60e12cea302001c8595a82d890c6075980664649
Kabi <gadalkabi@gmail.com>
2021-12-17 11:03:22 +0530

    pulling changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
-import { Main } from './components/Cart/Main';
-import { AddressCard } from './components/deliveryAddressPage/AddressCard';
+
 import { DeliveryCard } from './components/DeliveryPage/main';
 import AppRoutes from './Routes/AppRoutes';
 
 function App() {
   return (
     <div className="App">
-      {/* <AppRoutes/> */}
+      <AppRoutes/>
       <DeliveryCard/>
     </div>
   );
