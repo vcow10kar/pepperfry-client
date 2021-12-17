93cd0ee32012ffc7b162e78afa7ad27346338086
Kabi <gadalkabi@gmail.com>
2021-12-17 12:29:05 +0530

    modification in css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 
-import { DeliveryCard } from './components/DeliveryPage/main';
+
 import AppRoutes from './Routes/AppRoutes';
 
 function App() {
   return (
     <div className="App">
       <AppRoutes/>
-      {/* <DeliveryCard/> */}
     </div>
   );
 }
